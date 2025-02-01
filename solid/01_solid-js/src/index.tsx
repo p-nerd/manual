9aa752a3f5c84c41449f9f25c619068297df04b9
Shihab Mahamud <shihab4t@gmail.com>
2025-02-02 04:05:42 +0600

    cleanup

--- solid/01_solid-js/src/index.tsx
+++ solid/01_solid-js/src/index.tsx
@@ -1,8 +1,11 @@
-/* @refresh reload */
-import { render } from "solid-js/web";
-import App from "./App.tsx";
 import "./index.css";
+
+import { render } from "solid-js/web";
+
+import App from "./App.tsx";
 
 const root = document.getElementById("root");
 
-render(() => <App />, root!);
+if (root) {
+    render(() => <App />, root);
+}
