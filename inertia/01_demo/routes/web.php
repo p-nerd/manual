510fd16c8ad118ec89ca26b3ae877a285f3c3059
Shihab Mahamud <shihab4t@gmail.com>
2025-03-24 23:48:37 +0600

    inertia server side setup

--- inertia/01_demo/routes/web.php
+++ inertia/01_demo/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', function () {
+    return Inertia::render('posts/index');
+})->name('posts.store');
